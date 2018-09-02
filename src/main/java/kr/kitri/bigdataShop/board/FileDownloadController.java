f1000dc72335c1bc3c21793e706fb4ed411d716b
dy <dy@222.98.207.37>
2018-09-03 00:56:13 +0900

    dyd

--- src/main/java/kr/kitri/bigdataShop/board/FileDownloadController.java
+++ src/main/java/kr/kitri/bigdataShop/board/FileDownloadController.java
@@ -5,6 +5,7 @@ import java.io.File;
 import org.apache.tiles.request.ApplicationContext;
 import org.apache.tiles.request.ApplicationContextAware;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,21 +13,21 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class FileDownloadController implements ApplicationContextAware{
-		private WebApplicationContext context = null;
-     
-	    @RequestMapping("/board/download.do")
-	    public ModelAndView download(@RequestParam("path")String path,
-	                                  @RequestParam("fileName")String fileName){
-	        String fullPath = path + "\\" + fileName;
-	        File file = new File(fullPath);
-	        //ModelAndView(String viewName, String modelName, Object modelObject) 
-	        return new ModelAndView("download", "downloadFile", file);
-	    }
-	 
-	    @Override
-	    public void setApplicationContext(ApplicationContext arg0)
-	            throws BeansException {
-	        this.context = (WebApplicationContext)arg0;
-	    }
+public class FileDownloadController implements ApplicationContextAware {
+	@Autowired
+	FileDownloadLogic downloadservice;
+	
+	private WebApplicationContext context = null;
+
+	@RequestMapping("/board/download.do")
+	public ModelAndView download(@RequestParam("path") String path, @RequestParam("fileName") String fileName) {
+		String fullPath = path + "\\" + fileName;
+		File file = new File(fullPath);
+		return new ModelAndView("download", "downloadFile", file);
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext arg0) throws BeansException {
+		this.context = (WebApplicationContext) arg0;
+	}
 }
